b34d6a846fd75b072091a17f91d34a4818a6f705
Mia <115048118+miapartington@users.noreply.github.com>
2024-07-06 17:52:47 -0400

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,23 +9,23 @@ const Navbar = () => {
     <nav className="navbar">
       <h1>Bookbabble</h1>
       <div className="links">
-        <a href="/" exact>Home</a>
-        <a href="/About">About</a>
-        <a href="/Blog">Blog</a>
+        <a href="/Bookbabble/" exact>Home</a>
+        <a href="/Bookbabble/About">About</a>
+        <a href="/Bookbabble/Blog">Blog</a>
         <div
           className="dropdown"
           onMouseEnter={() => setDropdownVisible(true)}
           onMouseLeave={() => setDropdownVisible(false)}
         >
-          <a href="/Marketplace">Marketplace</a>
+          <a href="/Bookbabble/Marketplace">Marketplace</a>
           {dropdownVisible && (
             <div className="dropdown-content">
               <div className="dropdown-column">
                 <h3>Categories</h3>
-                <a href="/Sales">New</a>
-                <a href="/Romance">Romance</a>
-                <a href="/Action">Action</a>
-                <a href="/Education">Education</a>
+                <a href="/Bookbabble/Sales">New</a>
+                <a href="/Bookbabble/Romance">Romance</a>
+                <a href="/Bookbabble/Action">Action</a>
+                <a href="/Bookbabble/Education">Education</a>
               </div>
             </div>
           )}
